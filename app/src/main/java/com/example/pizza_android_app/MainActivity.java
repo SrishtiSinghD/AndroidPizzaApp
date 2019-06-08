a3915870706dcb89af47587f8656d83f7fdc0522
Srishti Singh <srishtisingh20014@gmail.com>
2019-06-08 15:38:00 +0530

    Call button widget working now

--- app/src/main/java/com/example/pizza_android_app/MainActivity.java
+++ app/src/main/java/com/example/pizza_android_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pizza_android_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String number="8958998363";
         btsee=(Button)findViewById(R.id.see);
         btview=(Button)findViewById(R.id.view);
         btcall=(Button)findViewById(R.id.call);
@@ -33,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
             {
                 Intent intentview=new Intent(MainActivity.this, pizzaspinner.class);
                 startActivity(intentview);
+            }
+        });
+        btcall.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+               Intent intentcall= new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+number));
+               startActivity(intentcall);
             }
         });
     }
